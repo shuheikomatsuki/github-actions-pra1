65e3174fa9ba459d8a15b664ad96e881d36f6bab
shuheikomatsuki <shuheijy@gmail.com>
2025-09-05 15:13:40 +0900

    間違っている関数をコメントアウト

--- sample.c
+++ sample.c
@@ -5,13 +5,13 @@ int addCorrect(int a, int b) {
     return a + b;
 }
 
-int addWrong(int a, int b) {
-    return a - b;
-}
+// int addWrong(int a, int b) {
+//     return a - b;
+// }
 
 int subCorrect(int a, int b) {
     return a - b;
 }
-int subWrong(int a, int b) {
-    return a + b;
-}
+// int subWrong(int a, int b) {
+//     return a + b;
+// }
